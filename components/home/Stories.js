45477cc1b4e26534d7bba6e22d34a1697879c6e2
AayushMohan <aayushmohan1702@gmail.com>
2022-07-23 23:12:29 +0530

    Changed: User Name

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -7,7 +7,7 @@ export const USERS = [
     image: "https://avatars.githubusercontent.com/u/66319691?v=4",
   },
   {
-    name: "billgates",
+    name: "thisisbillgates",
     image:
       "https://instagram.fdel3-4.fna.fbcdn.net/v/t51.2885-15/279475218_2505435586262800_6888583147541363357_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdel3-4.fna.fbcdn.net&_nc_cat=104&_nc_ohc=xmnJ4-r7hhoAX92d9By&tn=64ifc0vJpcqLTLfl&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT-BB6ZZr42PNpx8qAWXG-Rv_7KazWSyu2HKW8VKMaBEGg&oe=62E2B04F&_nc_sid=acd11b",
   },
