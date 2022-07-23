d81730d0ba0513465f9be7f64f34a06d8a2f6355
AayushMohan <aayushmohan1702@gmail.com>
2022-07-23 13:22:17 +0530

    Added: Icons Container

--- components/home/Header.js
+++ components/home/Header.js
@@ -3,13 +3,18 @@ import React from "react";
 
 const Header = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity>
         <Image
           style={styles.logo}
           source={require("../../assets/header-logo.png")}
         />
       </TouchableOpacity>
+      <View style={styles.iconsContainer}>
+        <Text style={{ color: "white" }}>Dummy </Text>
+        <Text style={{ color: "white" }}>Dummy </Text>
+        <Text style={{ color: "white" }}>Dummy </Text>
+      </View>
     </View>
   );
 };
@@ -17,6 +22,17 @@ const Header = () => {
 export default Header;
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 20,
+  },
+
+  iconsContainer: {
+    flexDirection: "row",
+  },
+
   logo: {
     width: 100,
     height: 50,
