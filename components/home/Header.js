ca8d05520e355f7af110aae6e05920babeb801f1
AayushMohan <aayushmohan1702@gmail.com>
2022-07-23 15:05:20 +0530

    Added: Unread Badge

--- components/home/Header.js
+++ components/home/Header.js
@@ -20,6 +20,9 @@ const Header = () => {
           />
         </TouchableOpacity>
         <TouchableOpacity>
+          <View style={styles.unreadBadge}>
+            <Text style={styles.unreadBadgeText}>11</Text>
+          </View>
           <Image
             source={{
               uri: "https://img.icons8.com/fluency-systems-regular/600/ffffff/facebook-messenger.png",
@@ -56,5 +59,22 @@ const styles = StyleSheet.create({
     height: 30,
     resizeMode: "contain",
     marginLeft: 10,
+  },
+  unreadBadge: {
+    backgroundColor: "#ff3250",
+    position: "absolute",
+    left: 25,
+    bottom: 18,
+    width: 25,
+    height: 25,
+    borderRadius: 25,
+    alignItems: "center",
+    zIndex: 100,
+  },
+  unreadBadgeText: {
+    color: "white",
+    fontWeight: "600",
+    position: "absolute",
+    top: 3,
   },
 });
