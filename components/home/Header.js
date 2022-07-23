38bdeee39b96703348003356b1c936c9b6841c3e
AayushMohan <aayushmohan1702@gmail.com>
2022-07-23 13:11:13 +0530

    new file:   assets/header-logo.png 	modified:   components/home/Header.js

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 
 const Header = () => {
   return (
     <View>
-      <Text style={{ color: "white" }}>Header</Text>
+      <TouchableOpacity>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/header-logo.png")}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  logo: {
+    width: 100,
+    height: 50,
+    resizeMode: "contain",
+  },
+});
