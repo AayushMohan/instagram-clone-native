751d125b0f96ba2acc3dc683a5e0aaf4570729ab
AayushMohan <aayushmohan1702@gmail.com>
2022-07-25 12:31:49 +0530

    Added: Users Data

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,21 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
-export const USERS = [
-  {
-    name: "thisisaayushmohan",
-    image: "https://avatars.githubusercontent.com/u/66319691?v=4",
-  },
-  {
-    name: "thisisbillgates",
-    image:
-      "https://instagram.fdel3-4.fna.fbcdn.net/v/t51.2885-15/279475218_2505435586262800_6888583147541363357_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdel3-4.fna.fbcdn.net&_nc_cat=104&_nc_ohc=xmnJ4-r7hhoAX92d9By&tn=64ifc0vJpcqLTLfl&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT-BB6ZZr42PNpx8qAWXG-Rv_7KazWSyu2HKW8VKMaBEGg&oe=62E2B04F&_nc_sid=acd11b",
-  },
-];
+import { USERS } from "../../data/users";
 
 const Stories = () => {
   return (
-    <View>
+    <View style={{ marginBottom: 13 }}>
       <Text style={{ color: "white" }}>Stories</Text>
     </View>
   );
---
+++ data/users.js
@@ -0,0 +1,17 @@
+export const USERS = [
+  {
+    name: "thisisaayushmohan",
+    image:
+      "https://instagram.fdel3-2.fna.fbcdn.net/v/t51.2885-19/271278320_2230211373796201_3550252510377996040_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fdel3-2.fna.fbcdn.net&_nc_cat=101&_nc_ohc=AxrXZMy-Zm4AX_mG-iR&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT8v0vBidCtNP8FWxOoD9k90zi1A-79ChAioDeZbx2Y1rA&oe=62E52483&_nc_sid=8fd12b",
+  },
+  {
+    name: "thisisbillgates",
+    image:
+      "https://instagram.fdel3-4.fna.fbcdn.net/v/t51.2885-15/279475218_2505435586262800_6888583147541363357_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdel3-4.fna.fbcdn.net&_nc_cat=104&_nc_ohc=xmnJ4-r7hhoAX92d9By&tn=64ifc0vJpcqLTLfl&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT-BB6ZZr42PNpx8qAWXG-Rv_7KazWSyu2HKW8VKMaBEGg&oe=62E2B04F&_nc_sid=acd11b",
+  },
+  {
+    name: "aayushcodes",
+    image:
+      "https://instagram.fdel3-2.fna.fbcdn.net/v/t51.2885-19/289057432_1577160816011497_2805460423562490296_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fdel3-2.fna.fbcdn.net&_nc_cat=101&_nc_ohc=sIQaSe7HrUQAX-VsUDp&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT8jcLUWbg4bc1W4go41aJbToPqBmY4byz5TE_Y-n0XdzQ&oe=62E4D1C1&_nc_sid=8fd12b",
+  },
+];
