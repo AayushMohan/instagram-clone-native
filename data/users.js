4d794473f573cf8e5c8aed23e40d8236e3cf94f3
AayushMohan <aayushmohan1702@gmail.com>
2022-07-25 20:45:03 +0530

    Updated: Likes Count

--- data/users.js
+++ data/users.js
@@ -1,12 +1,12 @@
 export const USERS = [
   {
-    user: "aayushmohan",
+    user: "thisisaayushmohan",
     image:
       "https://instagram.fdel3-2.fna.fbcdn.net/v/t51.2885-19/271278320_2230211373796201_3550252510377996040_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fdel3-2.fna.fbcdn.net&_nc_cat=101&_nc_ohc=AxrXZMy-Zm4AX_mG-iR&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT8v0vBidCtNP8FWxOoD9k90zi1A-79ChAioDeZbx2Y1rA&oe=62E52483&_nc_sid=8fd12b",
   },
 
   {
-    user: "billgates",
+    user: "thisisbillgates",
     image:
       "https://instagram.fdel3-4.fna.fbcdn.net/v/t51.2885-15/279475218_2505435586262800_6888583147541363357_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdel3-4.fna.fbcdn.net&_nc_cat=104&_nc_ohc=xmnJ4-r7hhoAX92d9By&tn=64ifc0vJpcqLTLfl&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT-BB6ZZr42PNpx8qAWXG-Rv_7KazWSyu2HKW8VKMaBEGg&oe=62E2B04F&_nc_sid=acd11b",
   },
