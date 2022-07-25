a6916476baf5e70a0aa0af84a42f88a093eab1bc
AayushMohan <aayushmohan1702@gmail.com>
2022-07-25 16:05:50 +0530

    Added: Post Header

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Divider from "rn-dividers";
 
-const Post = (post) => {
+const Post = ({ post }) => {
   return (
     <View style={{ marginBottom: 30 }}>
-      <Text style={{ color: "white" }}>Post</Text>
+      <Divider width={1} orientation="vertical" />
+      <PostHeader post={post} />
     </View>
   );
 };
+
+const PostHeader = ({ post }) => (
+  <View>
+    <View>
+      <Image />
+      <Text></Text>
+    </View>
+  </View>
+);
 
 export default Post;
 
